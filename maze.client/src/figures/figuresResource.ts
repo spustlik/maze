3bd2bfa7875f649cb70c1205b6d20e1bf4a1db06
Jan Stuchlik <j.stuchlik@outlook.cz>
2024-05-20 19:26:05 +0200

    d10 d11

--- maze.client/src/figures/figuresResource.ts
+++ maze.client/src/figures/figuresResource.ts
@@ -17,6 +17,8 @@ export const figuresResources = new class BatmanResources {
     //D6 = new ex.ImageSource(path + "d6.png")
     _D7 = new ex.ImageSource(path + "d7.png")
     _D8 = new ex.ImageSource(path + "d8.png")
+    _D10 = new ex.ImageSource(path + "d10.png")
+    _D11 = new ex.ImageSource(path + "d11.png")
     _Plan = new ex.ImageSource(path + "plan.png")
     PlanGlow = new ex.ImageSource(path + "plan_glow.png")
     _Pointer = new ex.ImageSource(path + "pointer.png")
@@ -92,47 +94,51 @@ class FiguresData {
         const t = type_map[type];
         return this._plan_Sheet.getSprite(joints % 8, t * 2 + Math.trunc(joints / 8));
     }
-    private _sheet(opts: ex.SpriteSheetGridOptions & { offset?: ex.Vector }) {
-        return { ss: ex.SpriteSheet.fromImageSource(opts), ofs: opts.offset };
+    private _sheet(color:FigureColor, opts: ex.SpriteSheetGridOptions & { offset?: ex.Vector, delay?: number }) {
+        return { color, sheet: ex.SpriteSheet.fromImageSource(opts), ofs: opts.offset, delay: opts.delay ?? 50 };
     }
     private _dieAnim_Sheets = [
-        //blue
-        this._sheet({ image: figuresResources._D1, grid: { columns: 8, rows: 4, spriteWidth: 47, spriteHeight: 56 }, offset: ex.vec(-5, -17) }),
-        this._sheet({ image: figuresResources._D7, grid: { columns: 8, rows: 4, spriteWidth: 60, spriteHeight: 62 }, offset: ex.vec(1, -16) }),
-        this._sheet({ image: figuresResources._D8, grid: { columns: 8, rows: 4, spriteWidth: 78, spriteHeight: 87 }, offset: ex.vec(0, -5) }),
-        //green
-        this._sheet({ image: figuresResources._D2, grid: { columns: 8, rows: 4, spriteWidth: 47, spriteHeight: 56 }, offset: ex.vec(-5, -17) }),
-        //yellow
-        this._sheet({ image: figuresResources._D3, grid: { columns: 8, rows: 4, spriteWidth: 80, spriteHeight: 67 }, offset: ex.vec(0, -11) }),
-        this._sheet({ image: figuresResources._D5, grid: { columns: 8, rows: 4, spriteWidth: 80, spriteHeight: 70 }, offset: ex.vec(1, -11) }),
-        //red
-        this._sheet({ image: figuresResources._D4, grid: { columns: 8, rows: 4, spriteWidth: 56, spriteHeight: 60 }, offset: ex.vec(-1, -14) }),
+        //blue - deflation
+        this._sheet('B', { image: figuresResources._D1, grid: { columns: 8, rows: 4, spriteWidth: 47, spriteHeight: 56 }, offset: ex.vec(-5, -17) }),
+        //blue - fireflies
+        this._sheet('B', { image: figuresResources._D7, grid: { columns: 8, rows: 4, spriteWidth: 60, spriteHeight: 62 }, offset: ex.vec(2, -16) }),
+        //blue - inflation
+        this._sheet('B', { image: figuresResources._D8, grid: { columns: 8, rows: 4, spriteWidth: 78, spriteHeight: 87 }, offset: ex.vec(0, -5) }),
+
+        //green - acid
+        this._sheet('G', { image: figuresResources._D2, grid: { columns: 8, rows: 4, spriteWidth: 47, spriteHeight: 56 }, offset: ex.vec(-5, -17) }),
+        //green - hole fall
+        this._sheet('G', { image: figuresResources._D10, grid: { columns: 8, rows: 4, spriteWidth: 80, spriteHeight: 80 }, offset: ex.vec(1, -16), delay:15 }),
+        //green - twist down
+        this._sheet('G', { image: figuresResources._D11, grid: { columns: 8, rows: 4, spriteWidth: 80, spriteHeight: 80 }, offset: ex.vec(1, -16), delay: 20 }),
+
+        //yellow - skeleton
+        this._sheet('Y', { image: figuresResources._D3, grid: { columns: 8, rows: 4, spriteWidth: 80, spriteHeight: 67 }, offset: ex.vec(0, -11) }),
+        //yellow - disintegration
+        this._sheet('Y', { image: figuresResources._D5, grid: { columns: 8, rows: 4, spriteWidth: 80, spriteHeight: 70 }, offset: ex.vec(1, -11) }),
+
+        //red - melt
+        this._sheet('R', { image: figuresResources._D4, grid: { columns: 8, rows: 4, spriteWidth: 56, spriteHeight: 60 }, offset: ex.vec(-1, -14) }),
 
         //nakrajet na kousky, asi vodorovne, kazdy odjede na jinou stranu
-        //propadne se do zeme
         //rozpadne se jako sklo
         //teleport do nebe, teoreticky i UFO
         //teleport ala SG kruhy
         //teleport ala StarTrek - particles, nebo halo eff
+        //ohen, spadne popel
+        //twist do tycky
+        //voda se roztece, pozor stejne jako RED
 
     ];
 
-    getDieAnimSpriteOptions(index: number, delay: number) {
-        let opts = this._dieAnim_Sheets[index];
-        let a = ex.Animation.fromSpriteSheet(opts.ss, ex.range(0, 8 * 4 - 1), delay, ex.AnimationStrategy.End);
-        return { anim: a, offset: opts.ofs };
-    }
     getDieAnim(c: FigureColor) {
-        let i = 0;
-        if (c == 'B')
-            i = ex.randomIntInRange(0, 2);
-        else if (c == 'G')
-            i = 3 + ex.randomIntInRange(0, 0);
-        else if (c == 'Y')
-            i = 4 + ex.randomIntInRange(0, 1);
-        else if (c == 'R')
-            i = 6 + ex.randomIntInRange(0, 0);
-        return this.getDieAnimSpriteOptions(i, 50);
+        let copts = this._dieAnim_Sheets.filter(a => a.color == c);
+        let r = ex.randomIntInRange(0, copts.length-1);
+        let opts = copts[r];
+        opts = this._dieAnim_Sheets[5];
+        //console.log('getDieAnim', c, r, copts.length, opts);
+        let a = ex.Animation.fromSpriteSheet(opts.sheet, ex.range(0, 8 * 4 - 1), opts.delay, ex.AnimationStrategy.End);
+        return { anim: a, offset: opts.ofs };
     }
     private _selection_Sheet = ex.SpriteSheet.fromImageSource({
         image: figuresResources._Selection,
